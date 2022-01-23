75cbf0ba126e2933253951f5ef71f71a5a131562
ihong5 <ihong3589@gmail.com>
2022-01-22 22:30:44 -0500

    attempt to fix firebase auth rejected

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebase: {
     apiKey: process.env['NG_APP_API_KEY'],
     authDomain: process.env['NG_APP_AUTH_DOMAIN'],
+    databaseURL: process.env['NG_APP_DATABASE_URL'],
     projectId: process.env['NG_APP_PROJECT_ID'],
     storageBucket: process.env['NG_APP_STORAGE_BUCKET'],
     messagingSenderId: process.env['NG_APP_MESSAGING_SENDER_ID'],
